9f052400f1f291f0d46e2439c3a3cca78e4cc0b0
C4ncino <193305@iberopuebla.mx>
2023-01-27 13:54:54 -0600

    Fisicas terminadas, Error en camaras

--- Assets/Scripts/PhysicRig.cs
+++ Assets/Scripts/PhysicRig.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PhysicRig : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform PlayerHead;
+    public Transform LeftHand;
+    public Transform RightHand;
+
+    public CapsuleCollider BodyCollider;
+
+    public ConfigurableJoint PlayerHeadJoint;
+    public ConfigurableJoint LeftHandJoint;
+    public ConfigurableJoint RightHandJoint;
+
+    public float MinHeight = 0.5f;
+    public float MaxHeight = 2;
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        BodyCollider.height = Mathf.Clamp(PlayerHead.localPosition.y, MinHeight, MaxHeight);
+        BodyCollider.center = new Vector3(PlayerHead.localPosition.x, BodyCollider.height / 2, PlayerHead.localPosition.z);
+
+        PlayerHeadJoint.targetPosition = PlayerHead.localPosition;
+        LeftHandJoint.targetPosition = LeftHand.localPosition;
+        RightHandJoint.targetPosition = RightHand.localPosition;
+
+        LeftHandJoint.targetRotation = LeftHand.localRotation;
+        RightHandJoint.targetRotation = RightHand.localRotation;
+    
     }
 }
