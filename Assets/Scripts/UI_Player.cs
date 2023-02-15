6b1142cdee6595091a39e6613af92394a8ffcc8d
C4ncino <193305@iberopuebla.mx>
2023-02-15 13:07:16 -0600

    Player Warnings ready

--- Assets/Scripts/UI_Player.cs
+++ Assets/Scripts/UI_Player.cs
@@ -1,31 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
 public class UI_Player : MonoBehaviour
 {
     private bool onEndPoint;
-    public GameObject player_UI;
-    public TMP_Text right_Text;
-    public TMP_Text left_Text;
+    // * Panel Principal
+    public GameObject playerUI;
+    // * Textos
+    public TMP_Text rightText;
+    public TMP_Text leftText;
+    // * Paneles de Advertencia
+    public GameObject rightLight;
+    public GameObject leftLight;
+    
     public Transform puntoFinal;
     public Transform PlayerHead;
+
+    public Color empty;
+    public Color red;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        onEndPoint = false;
+
+        // onEndPoint = false;
         Debug.Log("Hola");
+        // Renderer right = rightLight.GetComponent<Renderer>();
+        // Renderer left = leftLight.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Image right = rightLight.GetComponent<Image>();
+        Image left = leftLight.GetComponent<Image>();
         
-        float A = PlayerHead.position.x;
-        Debug.Log(A);
+        float headDeviation = PlayerHead.position.x;
+        Debug.Log(headDeviation);
+        if (headDeviation == 0){
+            right.color = empty;
+            left.color = empty;
+            
+            rightText.text = "";
+            leftText.text = "";
+        }
+        else if (headDeviation < 0){
+            right.color = empty;
+            left.color = red;
+
+            rightText.text = "";
+            leftText.text = "Estás muy cerca de tu izquierda";
+        }
+        else if (headDeviation > 0){
+            right.color = red;
+            left.color = empty;
+
+            rightText.text = "Estás muy cerca de tu derecha";
+            leftText.text = "";
+        }
         // if (!onEndPoint)
         // {
         //     float dist = Vector3.Distance(puntoFinal.position, this.transform.position);
