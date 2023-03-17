82092d4bd2c076bcf32d1f8a3344f8933f9637ca
C4ncino <193305@iberopuebla.mx>
2023-03-17 12:59:35 -0600

    Actualizacion de version

---
+++ Assets/Scripts/VRRig.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void Map(){
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+public class VRRig : MonoBehaviour
+{
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+
+    public Transform headConstraint;
+    public Transform Body;
+    public Vector3 headBodyOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        headBodyOffset = Body.position - headConstraint.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Body.position = headConstraint.position + headBodyOffset;
+        Body.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up);
+
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
+}
