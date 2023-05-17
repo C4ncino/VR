5a21b22f7856ba530f93d163cfca3b6d10e2edb2
C4ncino <193305@iberopuebla.mx>
2023-05-17 13:21:13 -0600

    Arduino COMS

--- Assets/Character/Scripts/MovementInput.cs
+++ Assets/Character/Scripts/MovementInput.cs
@@ -1,26 +1,29 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using System.IO.Ports;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
 
-// public class MovementInput : MonoBehaviour
-// {
-//     public string portName;
-//     public int baudRate;
-//     private SerialPort serialPort;
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         serialPort = new SerialPort(portName, baudRate);
-//     }
+public class MovementInput : MonoBehaviour
+{
+    public string portName;
+    public int baudRate;
+    public string msg;
+    private SerialPort serialPort;
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialPort = new SerialPort(portName, baudRate);
+        serialPort.Open();
+        serialPort.ReadTimeout = 100;
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if (serialPort.IsOpen){
-//             serialPort.Write("A");
-//             Debug.Log("A");
-//         }
+    // Update is called once per frame
+    void Update()
+    {
+        if (serialPort.IsOpen){
+            serialPort.Write(msg);
+            Debug.Log(msg);
+        }
         
-//     }
-// }
+    }
+}
